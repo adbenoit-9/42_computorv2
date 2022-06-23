122a800ea232f15f592a84a9a17d3c2b941bfeb4
Adeline BENOIT <adbenoit@e2r11p14.42.fr>
2022-06-23 17:40:59 +0200

    tester++

--- tester/tester.sh
+++ tester/tester.sh
@@ -1,8 +1,8 @@
-rm diff.txt 2> /dev/null
 cat tests.txt | python3 ../computorv2.py > tests_out.txt 2> /dev/null
 diff tests_out.txt output.txt > diff.txt
 if [ $? -eq 0 ]
 then
+    rm diff.txt 2> /dev/null
     echo '\033[1mcomputorv2 [\033[32mOK\033[1;30m]\033[0m'
 else
     cat diff.txt
