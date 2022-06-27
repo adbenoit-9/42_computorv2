ab29b63eef924513e93f0701cbafb57418671ddc
Adeline BENOIT <adbenoit@e1r11p14.42.fr>
2022-06-27 17:25:33 +0200

    ++tester

--- tester/tester.sh
+++ tester/tester.sh
@@ -3,9 +3,9 @@ diff tests_out.txt output.txt > diff.txt
 if [ $? -eq 0 ]
 then
     rm diff.txt 2> /dev/null
-    echo '\033[1mcomputorv2 [\033[32mOK\033[1;30m]\033[0m'
+    echo '\033[1mcomputorv2 [\033[32mOK\033[1;37m]\033[0m'
 else
     cat diff.txt
-    echo '\n\033[1mcomputorv2 [\033[31mKO\033[1;30m]\033[0m'
+    echo '\n\033[1mcomputorv2 [\033[31mKO\033[1;37m]\033[0m'
 fi
 rm tests_out.txt .computorv2_history
